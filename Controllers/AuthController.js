47b751c26f7c1a714ebc936fbe57c10aeca08d7d
Sarthak Narain <sarthaknarain27@gmail.com>
2024-03-30 01:11:05 +0530

    updated dependencies and controller

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -10,14 +10,14 @@ export const Signup = async (req, res, next) => {
       return res.json({ message: "User already exists" });
     }
     const user = await User.create({ email, password, firstname, lastname, address, createdAt });
-    const token = createSecretToken(user._id);
-    res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
-    });
+    // const token = createSecretToken(user._id);
+    // res.cookie("token", token, {
+    //   withCredentials: true,
+    //   httpOnly: false,
+    // });
     res
       .status(201)
-      .json({ message: "User signed in successfully", success: true, user });
+      .json({ message: "User registered successfully", success: true, user });
     next();
   } catch (error) {
     console.error(error);
