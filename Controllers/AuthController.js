7daef8287b44e5099d7ef2d2cc3226d63c0f89dc
devkumar27 <dev270502@gmail.com>
2024-04-26 23:54:30 +0530

    fix: samesite cookie err

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -67,7 +67,8 @@ export const Login = async (req, res, next) => {
         const token = createSecretToken(user._id);
         res.cookie("token", token, {
           withCredentials: true,
-          httpOnly: false,
+          httpOnly: true,
+          sameSite: secure ? 'None' : 'Lax',
         });
         res.status(201).json({ message: "User logged in successfully", success: true });
       } else if(admin) {
@@ -78,7 +79,8 @@ export const Login = async (req, res, next) => {
         const token = createSecretToken(admin._id);
         res.cookie("token", token, {
           withCredentials: true,
-          httpOnly: false,
+          httpOnly: true,
+          sameSite: secure ? 'None' : 'Lax',
         });
         res.status(201).json({ message: "Admin logged in successfully", success: true });
       }
